6fe9b4976b9835708fb01d55e5e15c9850b35cb2
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-06-28 17:22:08 +0900

    #3 스크롤 내리면 말풍선  인덱스에 따라 이미지 보여주기 & 활성화된 이미지만 보여주도록

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 
     const stepElement = document.querySelectorAll('.step');
     const graphElement = document.querySelectorAll('.graphic-item');
+    let currentItem; //현재 활성화된 visible 클래스가 붙은 graphic-item;
 
     for(let i =0; i <stepElement.length; i++) {
         // stepElement[i].setAttribute('data-index',i);
@@ -11,4 +12,30 @@
         graphElement[i].dataset.index = i;
     }
     
+    //스크롤 이벤트가 발생할 때 
+    window.addEventListener('scroll', () => {
+        //말풍선들의 위치 체크.
+        let step;
+        let boundingRect;
+        for(let i = 0; i< stepElement.length; i++) {
+            step=stepElement[i];
+            boundingRect = step.getBoundingClientRect();
+            
+            //창 사이즈 높이를 기준으로 100으로 했을 때 80% 정도 들어오면
+            
+            if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8) {
+                //몇 번 째 인덱스가 나오는지
+                // console.log(step.dataset.index);
+                if(currentItem) {
+                currentItem.classList.remove('visible');
+                }
+
+                currentItem = graphElement[step.dataset.index];
+                currentItem.classList.add('visible');
+            }
+
+        }
+    });
+
 })();
+
