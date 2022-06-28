171ca2d3f8637d990970ef08f15a6ceca30ca544
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-06-28 17:48:38 +0900

    #3 intersection observer 예시

--- main.js
+++ main.js
@@ -4,12 +4,33 @@
 
     const stepElement = document.querySelectorAll('.step');
     const graphElement = document.querySelectorAll('.graphic-item');
-    let currentItem; //현재 활성화된 visible 클래스가 붙은 graphic-item;
+    let currentItem = graphElement[0]; //현재 활성화된 visible 클래스가 붙은 graphic-item;
+    let ioIndex;
+    //눈에 보이게 됐는지, 사라졌는지, 어떤 비율에서 부터 보였는지 등등..다양함. 
+      //observer가 관찰을 하도록 등록을 해줘야함. io를 사용해서 관찰을 시키는 것.
+    const io = new IntersectionObserver((entries, observer)=> {
+        //중간에 0 찍히는데 이거는 사라지니까.
+        // 문자열 -> 숫자로 바꿔야
+        ioIndex = entries[0].target.dataset.index * 1;
+        // console.log(ioIndex);
+    });
+
 
     for(let i =0; i <stepElement.length; i++) {
         // stepElement[i].setAttribute('data-index',i);
+        io.observe(stepElement[i]); //모든 말풍선들이 관찰대상으로 등록됨. 
         stepElement[i].dataset.index = i;
         graphElement[i].dataset.index = i;
+    }
+
+    //Current visible 붙여주기
+    function activate() {
+        currentItem.classList.add('visible');
+    }
+
+    // "" 삭제
+    function inactivate() {
+        currentItem.classList.remove('visible');
     }
     
     //스크롤 이벤트가 발생할 때 
@@ -17,25 +38,27 @@
         //말풍선들의 위치 체크.
         let step;
         let boundingRect;
+        let temp = 0;
         for(let i = 0; i< stepElement.length; i++) {
             step=stepElement[i];
             boundingRect = step.getBoundingClientRect();
-            
+            temp++;
             //창 사이즈 높이를 기준으로 100으로 했을 때 80% 정도 들어오면
             
             if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8) {
                 //몇 번 째 인덱스가 나오는지
                 // console.log(step.dataset.index);
                 if(currentItem) {
-                currentItem.classList.remove('visible');
+                    inactivate();
                 }
 
                 currentItem = graphElement[step.dataset.index];
-                currentItem.classList.add('visible');
+                activate();
             }
-
         }
+        console.log(temp);
     });
 
+    activate();
 })();
 
